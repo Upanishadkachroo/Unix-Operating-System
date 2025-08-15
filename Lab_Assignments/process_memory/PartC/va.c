79a266011840df60fe10c3da2a45a041f5527954
Upanishadkachroo <upanishadkachroo@gmail.com>
2025-08-15 18:16:32 +0530

    worked on memory management in linux arch

--- Lab_Assignments/process_memory/PartC/va.c
+++ Lab_Assignments/process_memory/PartC/va.c
@@ -5,12 +5,26 @@
 
 int main(int argc, char * argv[]) {
 
+    //main is the function pointer
+    //main gives address of the code segement in your process virtual address
     printf("location of code : %p\n", main); 
+
+    //heap starts above the global data segment and grows towards higher data add.
+    //gives pointer to the virtual address
     printf("location of heap : %p\n", malloc(100e6));
 
+
+    //The stack is located near the top of the process's address space and grows downward toward lower addresses.
+    //Printing &x gives the virtual address of that stack slot.
     int x=3;
 
     printf("location of stack: %p\n", &x);
+
+
+
+
+    //the output only shows the virtual address of the program 
+    //To view the actual addresss use kernel APIs like /prrc/<PID>/pagemap
     return x;
 
 }
